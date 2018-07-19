1e6cd3be1b939e336c4d763f98b37736818af56b
tonsojun <josephjun@gmail.com>
2018-07-19 11:20:16 -0700

    Pseudocode uers.js

--- database/user.js
+++ database/user.js
@@ -8,7 +8,18 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-// TODO: Find specific user's favorite drinks
-User.findFavDrinks = () => {};
+// TODO: Find specific user's favorite drinks and attach drink object to favDrinks array
+User.findFavDrinks = (query, callback) => {
+  // check to see if MongoIDkey  is unique
+    // if unique
+      // add to favorites array
+    // if not unique
+      // throw err?
+
+    // call back
+      // return the favorte drink array
+
+};
+
 
 module.exports = User;
