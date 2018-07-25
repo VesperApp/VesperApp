68f644423db7f051c046dd0e6222f126a3ecb7f4
tonsojun <josephjun@gmail.com>
2018-07-25 14:02:30 -0700

    pull new code

--- database/user.js
+++ database/user.js
@@ -47,25 +47,25 @@ User.register = (req, cb) => {
 //     });
 //   })
 // };
-=======
 
-User.findFavDrinks = (query, callback) => {
-  // check to see if MongoIDkey  is unique
-  User.findById("5b50f96602b1850bd2fff872", (err, drink) => { // just use find?
-  // User.findById(req._id, (err, drink) => {
-    if(err) {console.log('there is an ', err)};
-    console.log('drink is', drink);
-    drink.name = 'joeytest';
-    drink.favDrinks[1] = 'secondTestDrink'
-    drink.save(function(err, favDrink) {
-      if(err) {
-        console.log('the save did not work', err);
-      } else {
-        // res.send(favDrink) // error saying res not defined??
-      }
-    });
-  })
-};
+
+// User.findFavDrinks = (query, callback) => {
+//   // check to see if MongoIDkey  is unique
+//   User.findById("5b50f96602b1850bd2fff872", (err, drink) => { // just use find?
+//   // User.findById(req._id, (err, drink) => {
+//     if(err) {console.log('there is an ', err)};
+//     console.log('drink is', drink);
+//     drink.name = 'joeytest';
+//     drink.favDrinks[1] = 'secondTestDrink'
+//     drink.save(function(err, favDrink) {
+//       if(err) {
+//         console.log('the save did not work', err);
+//       } else {
+//         // res.send(favDrink) // error saying res not defined??
+//       }
+//     });
+//   })
+// };
 
 // User.findFavDrinks();
 
