b374332298f0e4839227688e043f6fe7eda3d81e
e56 <edai28@gmail.com>
2018-09-14 21:40:30 -0400

    database relationships partial

--- database/associate.js
+++ database/associate.js
@@ -1,11 +1,18 @@
 const sequelize = require('./sequelize');
 const User = require('./Users');
-const Drink = require('./Drinks');
-const avorite = require('./Favorites');
+
+const DrinkMethods = require('./Drinks');
+const Drink = DrinkMethods.Drink;
+const Catagory = DrinkMethods.Catagory;
+const Glass = DrinkMethods.Glass;
+
+const Favorite = require('./Favorites');
 
 // User.belongsToMany(Drink, { through: 'Favorites' });
 User.hasMany(Drink, { foreignKey: 'creator_ID' });
 // Drink.belongsToMany(User, { through: 'Favorites' });
+Catagory.hasMany(Drink, { foreignKey: 'catagory_ID' });
+Glass.hasMany(Drink, { foreignKey: 'glass_ID' });
 
 sequelize.sync({force:true}).then(()=>{
     User.create({
