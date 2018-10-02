28d707e180e1118251559e363f94902dcd2d974f
Henry Han <potatoeater0127@gmail.com>
2018-10-02 11:13:42 -0700

    (fix) Comment out the sync() in associate.js and close the sequelize connection after tests done.

--- database/_tests_/util.js
+++ database/_tests_/util.js
@@ -1,10 +1,15 @@
 const { sequelize } = require('../associate');
 
 /**
- * Clear out  test database before and after all tests.
+ * Clear out  test database before all tests.
  */
 const truncateTables = () => sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
   .then(() => sequelize.sync({ force: true }))
   .then(() => sequelize.query('SET FOREIGN_KEY_CHECKS = 1'));
 
-module.exports = { truncateTables };
+/**
+ * Close the connection after tests done, otherwise jest would hang.
+ */
+const endConnection = () => sequelize.close();
+
+module.exports = { truncateTables, endConnection };
--- database/associate.js
+++ database/associate.js
@@ -18,50 +18,50 @@ Glass.hasMany(Drink, { foreignKey: 'glass_ID' });
 Drink.belongsToMany(Ingredient, { through: 'DrinkIngredient' });
 Ingredient.belongsToMany(Drink, { through: 'DrinkIngredient' });
 
-sequelize.sync({force:true}).then(()=>{
+// sequelize.sync({force:true}).then(()=>{
 
-    User.create({
-        user_name: "HI12345",
-        password: "Sequelize.STRING",
-    // picture_url: "Sequelize.STRING",
-    }).then(user => {
-        Drink.create({
-            drink_name: "Drinkname12334"
-        }).then(drink => 
-            user.setDrinks([drink]).then(data=>console.log("this is the data",data))
-        )
-    })
+//     User.create({
+//         user_name: "HI12345",
+//         password: "Sequelize.STRING",
+//     // picture_url: "Sequelize.STRING",
+//     }).then(user => {
+//         Drink.create({
+//             drink_name: "Drinkname12334"
+//         }).then(drink => 
+//             user.setDrinks([drink]).then(data=>console.log("this is the data",data))
+//         )
+//     })
 
-    User.create({
-        user_name:"heeheehee",
-        password: "securepassword"
-    })
+//     User.create({
+//         user_name:"heeheehee",
+//         password: "securepassword"
+//     })
 
-    //create a "shot" && "glass" catagory to associate it with a Drink:
-    // first create the catagory "shot"
-    Category.create({
-        category_name: "shots123"
-    }).then(
-        newCategory=> 
-            // first create the catagory "glass"
-            Glass.create({
-                glass_name: "Shot Glass123"
-            }).then(newGlass =>
-                //then create the Drink
-                Drink.create({
-                    drink_name: "drink name123",
-                    picture_url: "www.drinkpic123.com"
-                }).then(drink => 
-                  {  //associate the "shot" && "glass" with the "drink":
-                    newCategory.setDrinks([drink]).then(data=> console.log("this is the glass data:",data))
-                    newGlass.setDrinks([drink]).then(data=> console.log("this is the glass data:",data))}
-                )
-            )
-        )
+//     //create a "shot" && "glass" catagory to associate it with a Drink:
+//     // first create the catagory "shot"
+//     Category.create({
+//         category_name: "shots123"
+//     }).then(
+//         newCategory=> 
+//             // first create the catagory "glass"
+//             Glass.create({
+//                 glass_name: "Shot Glass123"
+//             }).then(newGlass =>
+//                 //then create the Drink
+//                 Drink.create({
+//                     drink_name: "drink name123",
+//                     picture_url: "www.drinkpic123.com"
+//                 }).then(drink => 
+//                   {  //associate the "shot" && "glass" with the "drink":
+//                     newCategory.setDrinks([drink]).then(data=> console.log("this is the glass data:",data))
+//                     newGlass.setDrinks([drink]).then(data=> console.log("this is the glass data:",data))}
+//                 )
+//             )
+//         )
 
 
 
-}) 
+// }) 
 
 module.exports = {
   sequelize,
