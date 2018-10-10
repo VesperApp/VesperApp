e8a616cbccd51d8f6212fa68030ee58610ead58c
e56 <edai28@gmail.com>
2018-10-10 14:33:18 -0400

    finish search query database function

--- database/queryRoutes.js
+++ database/queryRoutes.js
@@ -14,6 +14,35 @@ const getDrinksFromIngredients = async ingredients => {
     }
   });
 
+  const results = await sequelize.query(
+    `SELECT DrinkIngredients.DrinkID, Drinks.drink_name, count(*) 
+      FROM DrinkIngredients INNER JOIN drinks 
+      ON DrinkIngredients.DrinkID=Drinks.id 
+      WHERE ${ingredientQueryFragment} 
+      GROUP BY DrinkId ORDER BY count(*) DESC LIMIT 10;`,
+    { model: Drink }
+  );
+
+  const finalResults = [];
+
+  // add associations to results:
+  await results.forEach(singleDrink => {
+    finalResults.push(
+      Drink.findById(singleDrink.dataValues.DrinkID, {
+        include: [{ model: Category }, { model: Ingredient }, { model: Glass }],
+      })
+    );
+  });
+
+  return Promise.all(finalResults);
+};
+
+module.exports = {
+  getDrinksFromIngredients,
+};
+
+// non async await pattern of 2nd half of query function:
+/* 
   return sequelize
     .query(
       `SELECT DrinkIngredients.DrinkID, Drinks.drink_name, count(*) 
@@ -34,18 +63,18 @@ const getDrinksFromIngredients = async ingredients => {
       });
       return Promise.all(executions);
     });
-};
+*/
 
-// async function hi() {
-//   let hi2 = await getDrinksFromIngredients([22, 39]);
-//   await console.log('*********');
-//   await console.log(JSON.stringify(hi2));
-//   await console.log('*********');
-//   return hi2;
-// }
+/* manual test of query function:   
+ 
+async function hi() {
+   let hi2 = await getDrinksFromIngredients([22, 39]);
+   await console.log('*********');
+   await console.log(JSON.stringify(hi2));
+   await console.log('*********');
+   return hi2;
+ }
 
-// hi();
+ hi();
 
-module.exports = {
-  getDrinksFromIngredients,
-};
+ */
