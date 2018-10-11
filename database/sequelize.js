dc67fae1c9d45d0903e3cdf0a1efc97aa642efde
e56 <edai28@gmail.com>
2018-10-10 22:26:31 -0400

    herku database

--- database/sequelize.js
+++ database/sequelize.js
@@ -3,13 +3,14 @@ const Sequelize = require('sequelize');
 let env;
 if (process.env.CLEARDB_DATABASE_URL) {
   env = 'heroku';
-} else if (process.env.NODE_ENV === 'test') {
-  env = 'test';
+  module.exports = new Sequelize(process.env.CLEARDB_DATABASE_URL);
 } else {
-  env = 'development';
+  if (process.env.NODE_ENV === 'test') {
+    env = 'test';
+  } else {
+    env = 'development';
+  }
+
+  const config = require('./config')[env];
+  module.exports = new Sequelize(config.database, config.username, config.password, config);
 }
-// const env = process.env.CLEARDB_DATABASE_URL ? 'heroku' : process.env.NODE_ENV === 'test' ? 'test' : 'development';
-
-const config = require('./config')[env];
-
-module.exports = new Sequelize(config.database, config.username, config.password, config);
